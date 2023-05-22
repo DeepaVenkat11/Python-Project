df901e97e4a66cb6aa3993d8fbdf1848a8df322f
Deepa_venkatesh <133554681+DeepaVenkat11@users.noreply.github.com>
2023-05-22 21:54:38 +0530

    Removed time configuration

--- Link_Generator.py
+++ Link_Generator.py
@@ -1,6 +1,7 @@
 import requests
-import time
 from bs4 import BeautifulSoup
+
+
 url=input("Enter a URL : ")
 links=[]
 website=requests.get(url)
@@ -10,5 +11,4 @@ for link in soup.find_all('a'):
     links.append(link.get('href'))
 for link in links:
     print(link)
-    time.sleep(1)
 print(len(links))
